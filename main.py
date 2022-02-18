1c184146c3b9f8d0ce1d58ac7832e3526080e00a
Oriel <33528173+Xcompanygames@users.noreply.github.com>
2022-02-18 18:18:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from data_collection_manager import data_collection
-from selenium import webdriver
-from article import articleScrapObject
 import os
+
 
 os.environ['PATH'] += r"C:/chromedriver"
 
 dc = data_collection(folder_name='test')
 
 print(dc.find_string('wild fruits and'))
+
+dc.close_driver()
